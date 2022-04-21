b796db4cbd43b0ce70e3316852315e9922cbb9c2
mamathasreepada <103158691+mamathasreepada@users.noreply.github.com>
2022-04-22 01:25:07 +1000

    Update BaseactionsPage.java
    extra comment added

--- CGU/src/main/java/pages/BaseactionsPage.java
+++ CGU/src/main/java/pages/BaseactionsPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Base.SetUp;
+//Extra comment for github
 
 public class BaseactionsPage extends SetUp {
 	public static void scrolldown()
